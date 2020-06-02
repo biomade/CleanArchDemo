a7e4661ce7a3d237c0b055ba7e813d925f6f4dc6
laurie kern <laurie.kern.ctr@us.af.mil>
2020-06-02 15:38:40 -0500

    migrate code to db

--- CleanArch.Solution/CleanArch.MVC/Startup.cs
+++ CleanArch.Solution/CleanArch.MVC/Startup.cs
@@ -29,7 +29,7 @@ namespace CleanArch.MVC
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("IdentityDBConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
